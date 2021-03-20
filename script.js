02e87121ba83471cc6f21e8f62651a025bc1fc22
Jani.Behm <jani.behm@gmail.com>
2021-03-21 01:13:40 +0200

    new commit

--- script.js
+++ script.js
@@ -12,36 +12,37 @@ window.onload = sendApiRequest()
 function useApiData(data) {
      
     //Select html elements
-    let question = document.querySelector('#question')
-    let btn_correct_answer = document.querySelector('#btn_correct_answer')
-    let btn_incorrect_answer0 = document.querySelector('#btn_incorrect_answer0')
-    let btn_incorrect_answer1 = document.querySelector('#btn_incorrect_answer1')
-    let btn_incorrect_answer2 = document.querySelector('#btn_incorrect_answer2')
+     let question = document.querySelector("#question")
+     let btn_correct_answer = document.querySelector('#btn_correct_answer')
+     let btn_incorrect_answer0 = document.querySelector('#btn_incorrect_answer0')
+     let btn_incorrect_answer1 = document.querySelector('#btn_incorrect_answer1')
+     let btn_incorrect_answer2 = document.querySelector('#btn_incorrect_answer2')
     
     
     //Create "question" text node
-    question.appendChild(document.createTextNode( `${data.results[0].question}`)); 
-
+    document.querySelector("#question").innerHTML = ` Question: ${data.results[0].question}` 
+   
     //ANSWERS
     
     //CORRECT ANSWER create text node
 
-    btn_correct_answer.appendChild(document.createTextNode( `${data.results[0].correct_answer}`)); 
+     btn_correct_answer.appendChild(document.createTextNode( `${data.results[0].correct_answer}`)); 
      
     //UNCORRECT ANSWERS create text node
      
-    btn_incorrect_answers0.appendChild(document.createTextNode( `${data.results[0].incorrect_answers[0]}`));  
+     btn_incorrect_answers0.appendChild(document.createTextNode( `${data.results[0].incorrect_answers[0]}`));  
     
-    btn_incorrect_answers1.appendChild(document.createTextNode( `${data.results[0].incorrect_answers[1]}`)); 
+     btn_incorrect_answers1.appendChild(document.createTextNode( `${data.results[0].incorrect_answers[1]}`)); 
 
-    btn_incorrect_answers2.appendChild(document.createTextNode( `${data.results[0].incorrect_answers[2]}`)); 
+     btn_incorrect_answers2.appendChild(document.createTextNode( `${data.results[0].incorrect_answers[2]}`)); 
     }
     //ANSWERS ends here
      
     
     
+
     //Select answers html element
-    let answers = document.querySelector('.answers');
+      let answers = document.querySelector('.answers');
     //Randomise button order
     for (let i = answers.children.length; i >= 0; i--) {
     answers.appendChild(answers.children[Math.random() * i | 0]);
@@ -54,12 +55,12 @@ function useApiData(data) {
     //      }
     //  }
 
-     btn_correct_answer.addEventListner("click", doSomething) 
+   
 
-      function doSomething(event){
-        console.log(event.type)//click
-        console.log(event.target)//target
-      }
+      // function doSomething(event){
+      //   console.log(event.type)//click
+      //   console.log(event.target)//target
+      // }
 
-        
+      // btn_correct_answer.addEventListner("click", doSomething)      
    
